55dd740506599a1af570e609c3d66dab4637f9d7
NicoleRau <nicole.rau@stud.fhgr.ch>
2024-10-08 10:15:34 +0200

    extractPubli erstellt

---
+++ etl/extractPubli.php
@@ -0,0 +1,26 @@
+<?php
+
+function fetchPubliData() {
+    $url = "https://api.publibike.ch/v1/public/partner/stations";
+
+    // Initialisiert eine cURL-Sitzung
+    $ch = curl_init($url);
+
+    // Setzt Optionen
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    // Führt die cURL-Sitzung aus und erhält den Inhalt
+    $response = curl_exec($ch);
+
+    // Schließt die cURL-Sitzung
+    curl_close($ch);
+
+    print_r($response);
+
+    // Dekodiert die JSON-Antwort und gibt Daten zurück
+    return json_decode($response, true);
+}
+
+// Gibt die Daten zurück, wenn dieses Skript eingebunden ist
+return fetchPubliData();
+?>
