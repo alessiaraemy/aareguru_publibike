cdba20d5f0e46b591469130c56758bce29decad4
NicoleRau <nicole.rau@stud.fhgr.ch>
2024-10-08 10:48:15 +0200

    transformPubli.php update

--- etl/transformPubli.php
+++ etl/transformPubli.php
@@ -1,40 +1,30 @@
 <?php
+// Daten von der API abrufen
 $data = include('extractPubli.php');
 
-// Bereite ein Array für die transformierten Daten vor
+// Initialisiere eine leere Liste, um die transformierten Daten zu speichern
 $transformedData = [];
 
-// Überprüfe, ob Daten vorhanden sind
+// Prüfen, ob Stationen-Daten vorhanden sind
 if (isset($data['stations']) && is_array($data['stations'])) {
+    // Durchlaufe jede Station und sammle die relevanten Informationen
     foreach ($data['stations'] as $station) {
-        // Bereite die Daten für jede Station vor
-        $stationRow = [
-            'Station ID' => $station['id'],
-            'Name' => $station['name'],
-            'Adresse' => $station['address'] . ', ' . $station['zip'] . ' ' . $station['city'],
-            'Fahrzeuge' => count($station['vehicles']), // Anzahl Fahrzeuge
-            'Kapazität' => $station['capacity'],
+        $currentData = [
+            'Location' => $station['name'] . ', ' . $station['city'],  // Kombiniere Name und Stadt der Station
+            'Latitude' => $station['latitude'],  // Latitude der Station
+            'Longitude' => $station['longitude'], // Longitude der Station
+            'Capacity' => $station['capacity'],   // Kapazität der Station
+            'Vehicles' => count($station['vehicles']) // Anzahl verfügbarer Fahrzeuge
         ];
 
-        // Optional: Fahrzeuge mit Batteriestand hinzufügen (falls verfügbar)
-        if (!empty($station['vehicles'])) {
-            $vehicleInfo = [];
-            foreach ($station['vehicles'] as $vehicle) {
-                $vehicleInfo[] = $vehicle['name'] . ' (Batterie: ' . ($vehicle['ebike_battery_level'] ?? 'n/a') . '%)';
-            }
-            $stationRow['Fahrzeugdetails'] = implode(', ', $vehicleInfo);
-        } else {
-            $stationRow['Fahrzeugdetails'] = 'Keine Fahrzeuge verfügbar';
-        }
+        // Speichere diese Daten in der transformierten Liste
+        $transformedData[] = $currentData;
     }
 }
 
-// Speichere diese Daten in der transformierten Liste
-$transformedData[] = $currentData;
-
 // Bereite eine Tabelle vor, um die Daten anzuzeigen
 echo "<table border='1'>";
-echo "<tr><th>Station ID</th><th>Name</th><th>Adresse</th><th>Fahrzeuge</th><th>Kapazität</th></tr>";
+echo "<tr><th>Location</th><th>Latitude</th><th>Longitude</th><th>Capacity</th><th>Vehicles</th></tr>";
 
 // Füge die transformierten Daten in die Tabelle ein
 foreach ($transformedData as $row) {
@@ -46,5 +36,5 @@ foreach ($transformedData as $row) {
 }
 
 echo "</table>";
-
 ?>
+
