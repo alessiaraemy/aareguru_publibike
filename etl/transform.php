2bb17c342a13e9d433ea83debb34c4950071e4e2
NicoleRau <nicole.rau@stud.fhgr.ch>
2024-10-08 10:40:47 +0200

    transform.php ausprobieren

--- etl/transform.php
+++ etl/transform.php
@@ -7,10 +7,9 @@ $transformedData = [];
 // Füge die aktuellen Daten hinzu (Ort, Temperatur, Fluss, etc.)
 $currentData = [
     'Location' => $data['aare']['location'],
-    'Coordinates' => 'Lat: ' . $data['aare']['coordinates']['lat'] . ', Lon: ' . $data['aare']['coordinates']['lon'],
+    'weatherTemperature' => $data['weather']['temperature'] . ' °C',
     'Temperature' => $data['aare']['temperature'] . ' °C',
     'Flow' => $data['aare']['flow'] . ' m³/s',
-    'Height' => $data['aare']['height'] . ' m'
 ];
 
 // Speichere diese Daten in der transformierten Liste
@@ -18,7 +17,7 @@ $transformedData[] = $currentData;
 
 // Bereite eine Tabelle vor, um die Daten anzuzeigen
 echo "<table border='1'>";
-echo "<tr><th>Location</th><th>Coordinates</th><th>Temperature</th><th>Flow</th><th>Height</th></tr>";
+echo "<tr><th>Location</th><th>weatherTemperature</th><th>Temperature</th><th>Flow</th>></tr>";
 
 // Füge die transformierten Daten in die Tabelle ein
 foreach ($transformedData as $row) {
