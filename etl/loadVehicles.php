7c487d661410d5811437aec2bb767435a4e7fc50
NicoleRau <nicole.rau@stud.fhgr.ch>
2024-10-10 11:15:14 +0200

    loadPubli.php und loadVehicles.php hinzugefügt

---
+++ etl/loadVehicles.php
@@ -0,0 +1,36 @@
+<?php
+
+// Transformations-Skript  als '230_transform.php' einbinden
+$jsonData = include('transformVehicles.php');
+
+// Dekodiert die JSON-Daten zu einem Array
+$dataArray = json_decode($jsonData, true);
+
+require_once 'config.php'; // Bindet die Datenbankkonfiguration ein
+
+try {
+    // Erstellt eine neue PDO-Instanz mit der Konfiguration aus config.php
+    $pdo = new PDO($dsn, $username, $password, $options);
+
+    // SQL-Query mit Platzhaltern für das Einfügen von Daten
+    $sql = "INSERT INTO vehicles (id, name, Gesamtzahl E-Bikes, Gesamtzahl Velos, location_id, Timestamp) VALUES (?, ?, ?, ?, ?, ?)";
+
+    // Bereitet die SQL-Anweisung vor
+    $stmt = $pdo->prepare($sql);
+
+    // Fügt jedes Element im Array in die Datenbank ein
+    foreach ($dataArray as $item) {
+        $stmt->execute([
+            $item['id'],
+            $item['name'],
+            $item['Gesamtzahl E-Bikes'],
+            $item['Gesamtzahl Velos'],
+            $item['location_id'],
+            $item['Timestamp'],
+        ]);
+    }
+
+    echo "Daten erfolgreich eingefügt.";
+} catch (PDOException $e) {
+    die("Verbindung zur Datenbank konnte nicht hergestellt werden: " . $e->getMessage());
+}
