9e9b5147cfac5dbce4ed449d1e44892710ee8650
NicoleRau <nicole.rau@stud.fhgr.ch>
2024-10-14 10:10:13 +0200

    main.js unloadPubli und unloadVehicles ergänzt

--- main.js
+++ main.js
@@ -1,27 +1,37 @@
-/*async function fetchStationData() {
-    try {
-        // Abrufen der Daten von unload.php
-        const response = await fetch('unload.php');
-
-        // Überprüfen, ob die Anfrage erfolgreich war
-        if (!response.ok) {
-            throw new Error('Fehler beim Abrufen der Daten');
-        }
-
-        // Die Daten als JSON parsen
-        const data = await response.json();
-
-        // Die Daten in der Konsole anzeigen
-        console.log(data);
-        
-    } catch (error) {
-        // Fehler in der Konsole anzeigen
-        console.error('Fehler:', error);
-    }
-}*/
-
  // AJAX Anfrage mit Fetch API
  fetch('etl/unload.php')
+ .then(response => {
+     if (!response.ok) {
+         throw new Error('Netzwerk-Antwort war nicht ok');
+     }
+     return response.json();  // Antwort in JSON umwandeln
+ })
+ .then(data => {
+     console.log(data);  // Daten in der Konsole anzeigen
+ })
+ .catch(error => {
+     console.error('Es gab ein Problem mit der Fetch-Operation:', error);
+ });
+
+
+  // AJAX Anfrage mit Fetch API
+  fetch('etl/unloadPubli.php')
+  .then(response => {
+      if (!response.ok) {
+          throw new Error('Netzwerk-Antwort war nicht ok');
+      }
+      return response.json();  // Antwort in JSON umwandeln
+  })
+  .then(data => {
+      console.log(data);  // Daten in der Konsole anzeigen
+  })
+  .catch(error => {
+      console.error('Es gab ein Problem mit der Fetch-Operation:', error);
+  });
+
+
+   // AJAX Anfrage mit Fetch API
+ fetch('etl/unloadVehicles.php')
  .then(response => {
      if (!response.ok) {
          throw new Error('Netzwerk-Antwort war nicht ok');
