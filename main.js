795380dd2754abe6a1b471cd5f1ffccf0dd3117f
NicoleRau <nicole.rau@stud.fhgr.ch>
2024-10-17 12:16:00 +0200

    verschiedene positionen geupdatet

--- main.js
+++ main.js
@@ -18,13 +18,13 @@ const positionData = [
     { id: 195, position: { top: "15%", left: "40%" }, infoBoxPosition: { top: "-40%", left: "80%" }}, //Lorrainebrücke
     { id: 663, position: { top: "25%", left: "15%" }, infoBoxPosition: { top: "-40%", left: "80%" }}, //Kleeplatz/ Bollwerk
     { id: 251, position: { top: "27%", left: "40%" }, infoBoxPosition: { top: "-40%", left: "80%" }}, //Kornhausplatz
-    { id: 898, position: { top: "17%", left: "65%" }, infoBoxPosition: { top: "-40%", left: "80%" }}, //Altenbergstrasse
-    { id: 254, position: { top: "30%", left: "65%" }, infoBoxPosition: { top: "-40%", left: "80%" }}, //Nydegg
+    { id: 898, position: { top: "17%", left: "65%" }, infoBoxPosition: { top: "-40%", left: "-250%" }}, //Altenbergstrasse
+    { id: 254, position: { top: "30%", left: "65%" }, infoBoxPosition: { top: "-40%", left: "-250%" }}, //Nydegg
     { id: 316, position: { top: "35%", left: "15%" }, infoBoxPosition: { top: "-40%", left: "80%" }}, //Marzilibahn
     { id: 315, position: { top: "45%", left: "15%" }, infoBoxPosition: { top: "-40%", left: "80%" }}, //Marzilibad
     { id: 478, position: { top: "50%", left: "25%" }, infoBoxPosition: { top: "-40%", left: "80%" }}, //Dampfzentrale
     { id: 321, position: { top: "55%", left: "12%" }, infoBoxPosition: { top: "-40%", left: "80%" }}, //Monbijoubrücke
-    { id: 312, position: { top: "50%", left: "70%" }, infoBoxPosition: { top: "-40%", left: "80%" }}, //Ka-We-De
+    { id: 312, position: { top: "50%", left: "70%" }, infoBoxPosition: { top: "-40%", left: "-250%" }}, //Ka-We-De
     { id: 323, position: { top: "52%", left: "80%" }, infoBoxPosition: { top: "-40%", left: "-280%" }}, //Restaurant Dählhölzli
     { id: 423, position: { top: "65%", left: "75%" }, infoBoxPosition: { top: "-40%", left: "-250%" }}, //Camping Eichholz
     { id: 326, position: { top: "30%", left: "92%" }, infoBoxPosition: { top: "-40%", left: "-230%" }} //Bärenpark
